3169385cf474ebf0ce7e9b417f5a6cea0ceb08e8
abwa07018-ship-it <abwa07018@gmail.com>
2026-04-17 23:19:47 +0300

    🛠️ Add screen stubs for navigation

---
+++ flutter_app/lib/screens/home_screen.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'feed/feed_screen.dart';
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+  @override
+  Widget build(BuildContext context) { return const FeedScreen(); }
+}
